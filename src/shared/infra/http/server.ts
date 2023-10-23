c58d546d471169eae25c4b4625c592d7e8eb0faa
Vinicius Carvalho <viniciuscarvalho60@yahoo.com.br>
2023-10-22 23:00:55 -0300

    Add port env

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import { app } from './app'
 import { createServer } from 'http'
 
-const server = createServer(app).listen(3000)
+const server = createServer(app).listen(process.env.PORT || 3000, () => {
+  console.log('Server is running.')
+})
 
 export { app, server }
